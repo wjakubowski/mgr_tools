9f5ebf2deda177f169cf588d189e775d3c7b6d43
Jakubowski Wojciech <plgwojciechjak@login01.pro.cyfronet.pl>
2018-12-02 19:35:35 +0100

    clarified data file header parsing, repaired 2d vector field ploiting

--- plotMagnetization.py
+++ plotMagnetization.py
@@ -3,18 +3,21 @@ matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
-import glob, sys
+import glob, os
 
 def readInfoDataFromFile(fileName):
 	dataInfo = {}
 	with open(fileName) as file:
 		for line in file:
+			if line[2:8] == "Desc: ":
+				line = line[:2] + line[9:]
 			if line[0] == "#":
-				splitedLine = line.split(":")
-				if len(splitedLine) == 2:
-					dataInfo[splitedLine[0][2:]] = splitedLine[1][1:-1]
-				if len(splitedLine) > 2:
-					dataInfo[splitedLine[0][2:]] = splitedLine[-1][:-1]
+				separatorPos = line.find(":")
+				if separatorPos != -1:
+					key, val = line[:separatorPos], line[separatorPos:]
+					key = key[2:]
+					val = val[2:-1]
+					dataInfo[key] = val
 				else:
 					pass
 			else:
@@ -46,43 +49,60 @@ def getXYsliceFrom3Ddata(nx, ny, z, data):
 		xy2DsliceYdata.append(yLinexy2DsliceYdatadata)
 	return [xy2DsliceXdata, xy2DsliceYdata]	
 	
-def animate(i, z, dataFiles, ax, fig):
+############################################################################################################
+
+def drow2DvectorField(fig, ax, xy2DsliceXdata, xy2DsliceYdata, dataInfo):
+	X, Y = np.meshgrid(np.arange(float(dataInfo["xmin"]), float(dataInfo["xmax"]), float(dataInfo["xstepsize"])), np.arange(float(dataInfo["xmin"]), float(dataInfo["xmax"]), float(dataInfo["xstepsize"])))
+	q=ax.quiver(X, Y, xy2DsliceXdata, xy2DsliceYdata)
+	ax.legend()
+	plt.title("{}\nsimulation time: {}".format(dataInfo["Title"], dataInfo["Stage simulation time"]))
+	# valuelabels = dataInfo["valuelabels"].split()
+	# valueunits = dataInfo["valueunits"].split()
+	plt.xlabel("{} [{}]".format("x", dataInfo["meshunit"]))
+	plt.ylabel("{} [{}]".format("y", dataInfo["meshunit"]))
+	xmin, xmax = float(dataInfo["xmin"]), float(dataInfo["xmax"])
+	ymin, ymax = float(dataInfo["ymin"]), float(dataInfo["ymax"])
+	plt.axis(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, style="sci")
+
+def drowAllMatched2DvectorFields(dataFileMatcher, z, outputDir):
+	dataFiles = glob.glob(dataFileMatcher)
+	fig, ax = plt.subplots()
+	for dataFile in dataFiles:
+		data = read3DdataFromFile(dataFile)
+		dataInfo = readInfoDataFromFile(dataFile)
+		[xy2DsliceXdata, xy2DsliceYdata ] = getXYsliceFrom3Ddata(int(dataInfo["xnodes"]), int(dataInfo["ynodes"]), z, data)
+		drow2DvectorField(fig, ax, xy2DsliceXdata, xy2DsliceYdata, dataInfo)
+		file = os.path.basename(dataFile)
+		file = file[:-4]
+		plt.savefig(outputDir+"/"+file+".png")
+		plt.close()
+	
+def animate2DvectoField(i, z, dataFiles, ax, fig):
 	ax.clear()
 	data = read3DdataFromFile(dataFiles[i])
 	dataInfo = readInfoDataFromFile(dataFiles[i])
-	[xy2DsliceXdata, xy2DsliceYdata ] = getXYsliceFrom3Ddata(dataInfo["xnodes"], dataInfo["ynodes"], z, data)
-	ax.quiver(xy2DsliceXdata, xy2DsliceYdata)
-	ax.legend()
-	plt.title("{}, iteracja: {}".format(dataInfo["Title"], i))
-	plt.xlabel("{} [{}]".format("spin_torque_x", "1/s"))
-	plt.ylabel("{} [{}]".format("spin_torque_y", "1/s"))
+	[xy2DsliceXdata, xy2DsliceYdata ] = getXYsliceFrom3Ddata(int(dataInfo["xnodes"]), int(dataInfo["ynodes"]), z, data)
+	drow2DvectorField(fig, ax, xy2DsliceXdata, xy2DsliceYdata, dataInfo)
 	
-	"""	x_step = float(dataInfo["xstepsize"])
-	x_nodes = int(dataInfo["xnodes"])
-	plt.xticks([x_step * x_num for x_num in range(x_nodes)])
-	plt.xlabel("{} [{}]".format("x", "m"))
-	"""
-	
-def drow2DsliceOfVectorField(strRegExMatchingFiles, z):
-	dataFiles = glob.glob(strRegExMatchingFiles);
-	print len(dataFiles)
-	for dataFile in dataFiles:
-		data = read3DdataFromFile(dataFile)	
-		[xy2DsliceXdata, xy2DsliceYdata ] = getXYsliceFrom3Ddata(z, data)
-		fig, ax = plt.subplots()
-		ax.quiver(xy2DsliceXdata, xy2DsliceYdata)
-		file = dataFile[:-4]
-		plt.savefig(file+".png")
-		plt.close()
-		
+def makeAnimation(dataFileMatcher, z, outputFile):
 	fig, ax = plt.subplots()
-	ani = animation.FuncAnimation(fig, animate, interval=1000, fargs=(z, dataFiles, ax, fig))
-	ani.save("ABCD2.mp4")
+	dataFiles = glob.glob(dataFileMatcher)
+	ani = animation.FuncAnimation(fig, animate2DvectoField, interval=1000, fargs=(z, dataFiles, ax, fig))
+	ani.save(outputFile)
 	plt.close()
 	
-Z=2
-#sys.argv[1]
-drow2DsliceOfVectorField("/net/scratch/people/plgwojciechjak/jednaBariera/freeAniz_20000_myfree_0.5000_mxfree_0.8660_mxtop_1_VProfileType_6_Voltage_0.1500_/*.ovf", Z)
+z=1
 
-print readInfoDataFromFile("/net/scratch/people/plgwojciechjak/jednaBariera/freeAniz_20000_myfree_0.5000_mxfree_0.8660_mxtop_1_VProfileType_6_Voltage_0.1500_/"
-	"jednaBariera-Oxs_TimeDriver-Magnetization-00-0000020.omf")
+# print readInfoDataFromFile("example_vector_output.omf")
+
+dataFiles = glob.glob("./example_vector_output.omf");
+data = read3DdataFromFile(dataFiles[0])	
+dataInfo = readInfoDataFromFile(dataFiles[0])
+[xy2DsliceXdata, xy2DsliceYdata] = getXYsliceFrom3Ddata(int(dataInfo["xnodes"]), int(dataInfo["ynodes"]), z, data)
+fig, ax = plt.subplots()
+drow2DvectorField(fig, ax, xy2DsliceXdata, xy2DsliceYdata, dataInfo)
+plt.savefig("zdzisek.png")
+
+makeAnimation("/net/archive/groups/plggspinsym/WJ/jednaBariera/myfree_0.5000_mxfree_0.8660_mxtop_1_VProfileType_6_Voltage_0.1500_/*.omf", z, "marian.mp4")
+
+# drowAllMatched2DvectorFields("/net/archive/groups/plggspinsym/WJ/jednaBariera/myfree_0.5000_mxfree_0.8660_mxtop_1_VProfileType_6_Voltage_0.1500_/*.omf", z, "./xxx")
