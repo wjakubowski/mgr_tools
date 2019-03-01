57be8aca45a4b7443fa9d781aa514fb551d4ad03
Jakubowski Wojciech <wojciech.jakubowski789@gmail.com>
2019-03-01 18:07:25 +0100

    introduced multiPlotMode to create plots with multiple lines

--- plotCharts.py
+++ plotCharts.py
@@ -1,5 +1,4 @@
 import os, re, sys
-from copy import copy
 import numpy as np
 import json
 
@@ -18,11 +17,13 @@ class Plotter():
 		self.odtFileName = odtFileName
 		self.plottingConfig = plottingConfig
 		self.headersMap = self.plottingConfig["headersMap"]
-		self.odtFileNames = [self.odtFileName]
 		self.odtFiles = {}
-		for odtFileName in self.odtFileNames:
-			self.odtFiles[odtFileName] = self.readOdtFile(odtFileName)
-		
+		if self.plottingConfig["multiPlotMode"]:
+			for	legendLabel, odtPath in plottingConfig["odtSources"].items():
+				self.odtFiles[odtPath] = self.readOdtFile(odtPath)
+		else:
+			self.odtFiles[odtFileName] = self.readOdtFile(self.odtFileName)
+
 	def readOdtFile(self, odtFileName):
 		odtColumnsDict = {}
 		with open(odtFileName) as odtFile:
@@ -60,78 +61,84 @@ class Plotter():
 		else: 
 			return headerName
 							
+	def drowSinglePlot(self, odtFileName, variableNameX, variableNameY, legendLabel = ""):
+		xDict = self.odtFiles[odtFileName][variableNameX]
+		yDict = self.odtFiles[odtFileName][variableNameY]
+		
+		x = xDict["data"]
+		xLabel = self.replaceHeader(variableNameX)
+		xUnit = self.replaceHeader(xDict["unit"])
+
+		y = yDict["data"]
+		yLabel = self.replaceHeader(variableNameY)
+		yUnit = self.replaceHeader(yDict["unit"])
+		
+		plt.plot(x, y, label = legendLabel)
+		plt.xlabel("{} [{}]".format(xLabel,xUnit))
+		plt.ylabel("{} [{}]".format(yLabel,yUnit))
+							
 	def drowFunctionOfVariables(self, variableNameX, variableNameY, outputFile):
-		with open(self.odtFileName) as odtFile:
-		
-			xDict = self.odtFiles[self.odtFileName][variableNameX]
-			yDict = self.odtFiles[self.odtFileName][variableNameY]
-		
-			x = xDict["data"]
-			xLabel = self.replaceHeader(variableNameX)
-			xUnit = self.replaceHeader(xDict["unit"])
+		if plottingConfig["multiPlotMode"]:
+			for legendLabel, odtPath in plottingConfig["odtSources"].items():
+				self.drowSinglePlot(odtPath, variableNameX, variableNameY, legendLabel)
+			plt.legend()
+		else:
+			self.drowSinglePlot(self.odtFileName, variableNameX, variableNameY)
+	
+		plt.savefig(outputFile)
+		plt.close()
 
-			y = yDict["data"]
-			yLabel = self.replaceHeader(variableNameY)
-			yUnit = self.replaceHeader(yDict["unit"])
-			
-			plt.plot(x, y)
-			plt.xlabel("{} [{}]".format(xLabel,xUnit))
-			plt.ylabel("{} [{}]".format(yLabel,yUnit))
-			plt.savefig(outputFile)
-			plt.close()
-
-	def animate(self, i, x, y, z, ax, fig):
+	"""def animate(self, i, x, y, z, ax, fig):
 		ax.clear()
 		ax.plot(x[:i], y[:i], z[:i])
 		ax.legend()
 		plt.xlabel("{} [1]".format("mx"))
 		plt.ylabel("{} [1]".format("my"))
-		ax.set_zlabel("{} [1]".format("mz"))
+		ax.set_zlabel("{} [1]".format("mz"))"""
 
 			
-	def drowTrajectory(self, variableNameX, variableNameY, variableNameZ, outputFile, framesPerSecond):
-		with open(self.odtFileName) as odtFile:
-			xDict = self.odtFiles[self.odtFileName][variableNameX]
-			yDict = self.odtFiles[self.odtFileName][variableNameY]
-			zDict = self.odtFiles[self.odtFileName][variableNameZ]
-			tDict = self.odtFiles[self.odtFileName]["{Oxs_TimeDriver::Simulation time}"]
+	def drowTrajectory(self, variableNameX, variableNameY, variableNameZ, outputFile):
+		xDict = self.odtFiles[self.odtFileName][variableNameX]
+		yDict = self.odtFiles[self.odtFileName][variableNameY]
+		zDict = self.odtFiles[self.odtFileName][variableNameZ]
+		tDict = self.odtFiles[self.odtFileName]["{Oxs_TimeDriver::Simulation time}"]
 		
-			x = xDict["data"]
-			xLabel = variableNameX
-			xUnit = xDict["unit"]
+		x = xDict["data"]
+		xLabel = variableNameX
+		xUnit = xDict["unit"]
 
-			y = yDict["data"]
-			yLabel = variableNameY
-			yUnit = yDict["unit"]
-			
-			z = zDict["data"]
-			zLabel = variableNameZ
-			zUnit = zDict["unit"]
-
-			t = tDict["data"]
-			tLabel = "{Oxs_TimeDriver::Simulation time}"
-			tUnit = tDict["unit"]
-			
-			fig = plt.figure()
-			ax = fig.gca(projection='3d')
-			ax.clear()
-			ax.plot(x, y, z)
-			ax.legend()
-			plt.xlabel("{} [{}]".format(xLabel,xUnit))
-			plt.ylabel("{} [{}]".format(yLabel,yUnit))
-			ax.set_zlabel("{} [{}]".format(zLabel,zUnit))
-			plt.savefig(outputFile)
-			plt.close()
+		y = yDict["data"]
+		yLabel = variableNameY
+		yUnit = yDict["unit"]
 		
-			"""fig = plt.figure()
-			ax = fig.gca(projection='3d')
-			ani = animation.FuncAnimation(fig, animate , frames = len(x), fargs = (x, y, z, ax, fig))
-			ani.save(outputFile+".mp4", fps=framesPerSecond)
-			plt.close()"""
+		z = zDict["data"]
+		zLabel = variableNameZ
+		zUnit = zDict["unit"]
+
+		t = tDict["data"]
+		tLabel = "{Oxs_TimeDriver::Simulation time}"
+		tUnit = tDict["unit"]
+		
+		fig = plt.figure()
+		ax = fig.gca(projection='3d')
+		ax.clear()
+		ax.plot(x, y, z)
+		ax.legend()
+		plt.xlabel("{} [{}]".format(xLabel,xUnit))
+		plt.ylabel("{} [{}]".format(yLabel,yUnit))
+		ax.set_zlabel("{} [{}]".format(zLabel,zUnit))
+		plt.savefig(outputFile)
+		plt.close()
+		
+		"""fig = plt.figure()
+		ax = fig.gca(projection='3d')
+		ani = animation.FuncAnimation(fig, animate , frames = len(x), fargs = (x, y, z, ax, fig))
+		ani.save(outputFile+".mp4", fps=int(self.plottingConfig["framesPerSecond"]))
+		plt.close()"""
 
 thisScritDirPath = os.getcwd()
-parametersPathElement = sys.argv[1]
-symulationName = sys.argv[2]
+symulationName = sys.argv[1]
+parametersPathElement = sys.argv[2]
 outputDataDirPath = thisScritDirPath+"/"+symulationName+"/"+parametersPathElement
 outputPlotsDirPath = thisScritDirPath+"/plots_tmp/"+symulationName+"/"+parametersPathElement
 outputDataOdtFilePath = outputDataDirPath+"/"+symulationName+".odt"
@@ -146,8 +153,7 @@ configFileStr = open(configFile).read()
 configFileJson = json.loads(configFileStr)
 
 plottingConfig = configFileJson["plot"]
-z=int(plottingConfig["zSlice"])
-framesPerSecond=int(plottingConfig["framesPerSecond"])
+"""z=int(plottingConfig["zSlice"])"""
 odtPlots=plottingConfig["odtPlots"]
 
 plotter = Plotter(outputDataOdtFilePath, plottingConfig)
@@ -156,10 +162,10 @@ for plotName, plotConfig in odtPlots.items():
 	if plotConfig["dim"] == "2D":
 		plotter.drowFunctionOfVariables(plotConfig["variableNameX"], plotConfig["variableNameY"] , plotConfig["outputFile"].format(outputPlotsDirPath))
 	elif plotConfig["dim"] == "3D":
-		plotter.drowTrajectory(plotConfig["variableNameX"], plotConfig["variableNameY"], plotConfig["variableNameZ"] , plotConfig["outputFile"].format(outputPlotsDirPath), framesPerSecond)
+		plotter.drowTrajectory(plotConfig["variableNameX"], plotConfig["variableNameY"], plotConfig["variableNameZ"] , plotConfig["outputFile"].format(outputPlotsDirPath))
 
 
-if plottingConfig["drawSingleFrames"]:
+"""if plottingConfig["drawSingleFrames"]:
 	try:
 		os.makedirs(outputPlotsDirPath+"/magnetization")
 	except:
@@ -169,7 +175,7 @@ if plottingConfig["drawSingleFrames"]:
 		os.makedirs(outputPlotsDirPath+"/spin_torque")
 	except:
 		pass
-	drowAllMatched2DvectorFields(outputDataDirPath+"/*.omf", z, outputPlotsDirPath+"/magnetization")
+	drowAllMatched2DvectorFields(outputDataDirPath+"/*.omf", z, outputPlotsDirPath+"/magnetization")"""
 
 """try:
 	makeAnimation(outputDataDirPath+"/*.omf", z, outputPlotsDirPath+"/magnetization.mp4")
